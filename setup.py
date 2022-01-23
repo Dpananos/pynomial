3150d862a37078e42581916891e3a9b9d2ff050e
Demetri <dpananos@gmail.com>
2022-01-23 13:00:13 -0500

    adds additional setup and config files

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='pynomial',
-    version='0.0.1',    
-    description='A python package for binomial confidence intervals',
-    url='https://github.com/Dpananos/pynomial',
-    author='Demetri Panano',
-    author_email='dpananos@gmail.com',
-    license='MIT',
-    packages=['pynomial'],
-    install_requires=['scipy>=1.0','numpy', 'pandas']
-)
+if __name__ == '__main__':
+    setup()
