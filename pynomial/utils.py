e737ffef5542eb01b7cecf19fc59e12ff78e0dc4
Demetri <dpananos@gmail.com>
2022-01-23 20:31:27 -0500

    adds skeleton for lrt intervals

--- pynomial/utils.py
+++ pynomial/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+from scipy.special import logit
+from scipy.optimize import newton
+from scipy.stats import binom, chi2
 
 def _check_args(x, n, conf):
 
@@ -8,7 +10,6 @@ def _check_args(x, n, conf):
 
     n_not_int = (isinstance(n, float) and not n.is_integer()) 
     n_not_int_arr = (isinstance(n, np.ndarray) and np.any(np.mod(n, 1)!=0))
-
 
 
     if x_not_int or x_not_int_arr:
@@ -23,3 +24,19 @@ def _check_args(x, n, conf):
     elif np.any(conf < 0) or np.any(conf > 1):
         raise ValueError("conf must be a number between 0 and 1")
 
+
+def _lrt_footfinding(x, n, conf):
+    alpha = 1-conf
+    X = chi2(df=1).ppf(1-alpha)
+
+    log_lik_est = binom(n=n, p=p).logpmf(x)
+
+    # For numerically stable reasons, we pass logit p to the log likelihood and 
+    # Perform the root finding in the log odds space
+    # Then, transform back to probability space
+    lrt_stat = lambda logit_p: -2*(binom(n=n, p=expit(logit_p)).logpmf(x) - log_lik_est) - X
+
+    logit_lower = newton(lrt_stat, x0 = logit(p) - 2, **kwargs)
+    logit_upper = newton(lrt_stat, x0 = logit(p) + 2, **kwargs)
+
+    return logit_lower, logit_upper
